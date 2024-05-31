dbc7699111cc09e75a8c88a24a3b72b3e6254153
cedar-c <54062513+cedar-c@users.noreply.github.com>
2024-05-31 11:20:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PROCESS_ID = YOUR_PROCESS;
 const app = express();
 
 const port = 3541;
+const sp = '：';
+let user = '';
 
 const wss = new WebSocket.Server({ noServer: true });
 const discordClient = new Client({
@@ -29,16 +31,35 @@ discordClient.login(token);
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
-        console.log("send to ao:", message.toString());
+        message = message.toString();
+        console.log('receive from aos', message);
+        if (message.split(sp)[0] !== 'noUser') {
+            user = message.split(sp)[0];
+        }else {
+            user = PROCESS_ID;
+            message = message.replace('noUser',user);
+        }
         const channel = discordClient.channels.cache.get(DISCORD_CHANNEL_ID);
         if (channel) {
-            channel.send(message.toString());
+            channel.send(message);
         }
     });
 
     discordClient.on('messageCreate', (message) => {
-        console.log("receive from discord, user:%s, content:%s", message.author.username, message.content)
-        sendMsg(message);
+        let name = message.author.username;
+        let content = message.content;
+        console.log('receive from discord, user:%s, content:%s', name, content);
+        //send by other aos user
+        if (content.includes(sp)) {
+            var split = content.split(sp);
+            if (split[0] !== user) {
+                name = split[0];
+                content = split[1];
+                sendMsg({name:name, content:content});
+            }
+        }else {
+            sendMsg({name:name, content:content});
+        }
     });
 });
 
@@ -54,31 +75,31 @@ server.on('upgrade', (request, socket, head) => {
 
 async function sendMsg(msg) {
 
-    const user = msg.author.username;
+    const user = msg.name;
     const content = msg.content;
 
     const wallet = JSON.parse(
-        readFileSync("/root/.aos.json").toString(),
+        readFileSync('/root/.aos.json').toString(),
     );
 
 // The only 2 mandatory parameters here are process and signer
     await message({
 
         /*
-          The arweave TXID of the process, this will become the "target".
+          The arweave TXID of the process, this will become the 'target'.
           This is the process the message is ultimately sent to.
         */
         process: PROCESS_ID,
         // Tags that the process will use as input.
         tags: [
-            {name: "Action", value: "ReceiveDiscord"},
-            {name: "Data", value: content},
-            {name: "User", value: user},
+            {name: 'Action', value: 'ReceiveDiscord'},
+            {name: 'Data', value: content},
+            {name: 'User', value: user},
         ],
-        // A signer function used to build the message "signature"
+        // A signer function used to build the message 'signature'
         signer: createDataItemSigner(wallet),
         /*
-          The "data" portion of the message
+          The 'data' portion of the message
           If not specified a random string will be generated
         */
         data: content,
