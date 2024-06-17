d9119534f3aed198a8a5a15f8d94aefa83735cd2
cedar-c <54062513+cedar-c@users.noreply.github.com>
2024-06-17 16:52:31 +0800

    Update chatroom.lua

--- chatroom.lua
+++ chatroom.lua
@@ -2,6 +2,6 @@ Handlers.add(
     "ReceiveDiscord",
     Handlers.utils.hasMatchingTag("Action", "ReceiveDiscord"),
     function(m)
-        Say(m.Data)
+        ao.send({Target = "6I1JBBc9SOMtqFxlX7OoYgsMh7QeZk2fFwUCHTUqshg", Action = "Broadcast", Send = "discord", NickName = m.User, Data = m.Data})
     end
 )
